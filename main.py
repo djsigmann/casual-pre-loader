faa47202982b2bbfdc4651273514b271cf4f7192
cueki <github2@madisonlovett.dev>
2024-12-02 14:52:26 -0800

    more main cleanup

--- main.py
+++ main.py
@@ -1,45 +1,12 @@
 import os
-import json
-from idlelib.pyshell import restart_line
-from pathlib import Path
-from typing import Tuple, List, Dict
-import matplotlib.pyplot as plt
 import yaml
-import argparse
-from dataclasses import dataclass
-from core.constants import PCF_OFFSETS, AttributeType
-from core.errors import PCFError
-from core.traversal import PCFTraversal
-from models.pcf_file import PCFFile
-from operations.color import color_shift, transform_with_shift, analyze_pcf_colors, transform_team_colors, RGB
-from operations.vpk import VPKOperations, VPKSearchResult
-from tools.color_wheel import plot_rgb_vector, animate_color_shift
 import random
+from typing import Dict
+from core.constants import PCF_OFFSETS
+from models.pcf_file import PCFFile
+from operations.color import analyze_pcf_colors, transform_team_colors, RGB
+from operations.vpk import VPKOperations
 
-@dataclass
-class ColorConfig:
-    red: [int, int, int]
-    blue: [int, int, int]
-
-
-@dataclass
-class PCFConfig:
-    vpk_file: str
-    pcf_file: str
-    colors: ColorConfig
-
-
-def load_config(config_path: str) -> PCFConfig:
-    with open(config_path, 'r') as f:
-        data = yaml.safe_load(f)
-        return PCFConfig(
-            vpk_file=data['vpk_file'],
-            pcf_file=data['pcf_file'],
-            colors=ColorConfig(
-                tuple(data['colors']['red']),
-                tuple(data['colors']['blue'])
-            )
-        )
 
 
 def generate_random_rgb():
@@ -55,19 +22,16 @@ def generate_random_targets():
         'red': {
             'color1': generate_random_rgb(),
             'color2': generate_random_rgb(),
-            'tint_clamp': (255, 255, 255),
             'color_fade': generate_random_rgb()
         },
         'blue': {
             'color1': generate_random_rgb(),
             'color2': generate_random_rgb(),
-            'tint_clamp': (255, 255, 255),
             'color_fade': generate_random_rgb()
         },
         'neutral': {
             'color1': generate_random_rgb(),
             'color2': generate_random_rgb(),
-            'tint_clamp': (255, 255, 255),
             'color_fade': generate_random_rgb()
         }
     }
@@ -81,7 +45,8 @@ def process_pcf(vpk_file: str, pcf_file: str, targets: Dict[str, Dict[str, RGB]]
     offset, size = PCF_OFFSETS.get(pcf_file)
 
     vpk_ops = VPKOperations
-    extracted_pcf = vpk_ops.extract_pcf(
+
+    vpk_ops.extract_pcf(
         vpk_path=vpk_file,
         offset=offset,
         size=size,
@@ -92,7 +57,7 @@ def process_pcf(vpk_file: str, pcf_file: str, targets: Dict[str, Dict[str, RGB]]
     colors = analyze_pcf_colors(pcf)
     result = transform_team_colors(pcf, colors, targets)
 
-    # animate_color_shift(colors, targets, save_video=False)
+    # animate_color_shift(colors, targets, save_video=False) # this is the color wheel animation, is broken rn
 
     result = vpk_ops.patch_pcf(
         vpk_path=vpk_file,
@@ -106,80 +71,13 @@ def process_pcf(vpk_file: str, pcf_file: str, targets: Dict[str, Dict[str, RGB]]
     os.remove(temp_pcf)
 
 
-def analyze_pcf_elements(pcf_path: Path):
-    pcf = PCFFile(pcf_path)
-    pcf.decode()
-    elements_info = []
-
-    for idx, element in enumerate(pcf.elements):
-        element_info = {
-            'index': idx,
-            'element_name': element.element_name,
-            'type_name_index': element.type_name_index,
-            'data_signature': element.data_signature.hex(),
-            'attributes': {}
-        }
-
-        for attr_name, (attr_type, value) in element.attributes.items():
-            element_info['attributes'][attr_name] = {
-                'type': attr_type.name,
-                'value': value
-            }
-
-        elements_info.append(element_info)
-
-    return elements_info
-
-
-def print_pcf_analysis(elements_info):
-    def convert_bytes(data):
-        if isinstance(data, bytes):
-            return data.decode('ascii', errors='replace')
-        if isinstance(data, dict):
-            return {convert_bytes(k): convert_bytes(v) for k, v in data.items()}
-        if isinstance(data, list):
-            return [convert_bytes(i) for i in data]
-        return data
-
-    cleaned_data = convert_bytes(elements_info)
-    json_object = json.dumps(cleaned_data, indent=2, default=str)
-    with open("sample.json", "w") as outfile:
-        outfile.write(json_object)
-
-
 def main():
     with open('config.yaml', 'r') as f:
         config = yaml.safe_load(f)
 
-    # red_target = 0, 255, 127
-    # blue_target = 255, 127, 0
-    # neutral_target = 127, 0, 255
-    # red_target = 0, 255, 0
-    # blue_target = 0, 255, 0
-    # neutral_target = 0, 255, 0
     vpk_file = config['vpk_file']
     pcf_files = config['pcf_files']
 
-    # targets = {
-    #     'red': {
-    #         'color1': (128, 255, 255),
-    #         'color2': (128, 255, 255),
-    #         'tint_clamp': (255, 255, 255),
-    #         'color_fade': (128, 192, 255)
-    #     },
-    #     'blue': {
-    #         'color1': (255, 128, 255),
-    #         'color2': (255, 128, 255),
-    #         'tint_clamp': (255, 255, 255),
-    #         'color_fade': (255, 128, 192)
-    #     },
-    #     'neutral': {
-    #         'color1': (255, 192, 128),
-    #         'color2': (255, 192, 128),
-    #         'tint_clamp': (255, 255, 255),
-    #         'color_fade': (192, 128, 255)
-    #     }
-    # }
     targets = {
         'red': {
             'color1': (255, 128, 128),
@@ -198,20 +96,15 @@ def main():
         }
     }
 
+    # DO ONLY WHATS IN CONFIG.YAML
     # for pcf_name in pcf_files:
-    #     # targets = generate_random_targets()
+    #     # targets = generate_random_targets() # if u want random
     #     process_pcf(vpk_file, pcf_name['file'], targets)
 
+    # DO ALL PARTICLE FILES !!!
     for pcf_name in PCF_OFFSETS:
-        # targets = generate_random_targets()
+        # targets = generate_random_targets() # if u want random
         process_pcf(vpk_file, pcf_name, targets)
-
-    # tracer = Path("rockettrail.pcf")
-    # tracer_pcf = PCFFile(tracer)
-    # tracer_pcf.decode()
-    # colors = analyze_pcf_colors(tracer_pcf, debug=True)
-    # result = transform_team_colors(tracer_pcf, colors, targets, debug=)
-    # print(f"Processed {tracer_pcf}: {result}")
 
 
 if __name__ == '__main__':
