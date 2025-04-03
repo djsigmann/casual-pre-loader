fbbd658c125a5bcc2b3c8fc5d9e70f42da2373b5
cueki <github2@madisonlovett.dev>
2025-04-02 22:43:12 -0700

    fix for ammopack_large

--- quickprecache/precache_list.py
+++ quickprecache/precache_list.py
@@ -29,6 +29,7 @@ def manage_folder(folder_path: Path) -> List[str]:
                 or "flag" in str(file_path).lower())
                 or "models/bots" in str(file_path).lower()
                 or "ammo_box" in str(file_path).lower()
+                or "ammopack" in str(file_path).lower()
                 or "medkit" in str(file_path).lower()):
             continue
 
@@ -63,6 +64,7 @@ def manage_vpk(vpk_path: Path) -> List[str]:
                     or "flag" in str(file_path).lower()
                     or "models/bots" in str(file_path).lower()
                     or "ammo_box" in str(file_path).lower()
+                    or "ammopack" in str(file_path).lower()
                     or "medkit" in str(file_path).lower()):
                 continue
 
