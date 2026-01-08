95b88893b365d26c51235f643264ae6883061dc6
djsigmann <djsigmann@protonmail.com>
2026-01-08 21:35:52 +0100

    fixup! refactor(util): add file submodule

--- core/util/file.py
+++ core/util/file.py
@@ -15,6 +15,15 @@ PathLike = Union[str, os.PathLike]
 
 
 def _get_next_new_file(file: Path) -> Path:
+    """
+    Atomically create a new file, appending underscores to its name until valid, and return a path-like object representing it.
+
+    Args:
+        file: The base file name.
+    Returns:
+        A path-like object representing the new file.
+    """
+
     while True:
         try:
             with file.open(os.O_CREAT | os.O_EXCL | os.O_WRONLY):
@@ -24,12 +33,30 @@ def _get_next_new_file(file: Path) -> Path:
 
 
 def pathify(path: PathLike) -> os.PathLike:
+    """
+    Convert an object to an absolute path-like object.
+
+    Args:
+        path: A path-like object or something that can be converted to one (i.e. a string like `"/usr/bin/sh"`).
+
+    Returns:
+        An absolute path-like object.
+    """
+
     if not isinstance(path, os.PathLike):
         path = Path(path)
     return path.resolve()
 
 
 def delete(file: PathLike, not_exist_ok: Optional[bool] = False) -> None:
+    """
+    Delete a file or directory.
+
+    Args:
+        file: The file to delete.
+        not_exist_ok: Do not throw an error if the file does not exist.
+    """
+
     file = pathify(file)
 
     try:
@@ -58,9 +85,27 @@ def copy(
     not_exist_ok: Optional[bool] = False,
     noclobber: Optional[bool] = False,
     ignore: Optional[Callable[[str, list[str]], Sequence]] = None,
-) -> Path:
+) -> Path | None:
+    """
+    Copy a file or directory.
+
+    Args:
+        src: The source file.
+        dst: The destination file.
+        not_exist_ok: Do not throw an error if the source does not exist.
+        noclobber: Throw an error if the destination exists (i.e. do not overwrite files).
+        ignore: A callable that is passed to the `ignore` argument of `shutil.copytree()`.
+
+    Returns:
+        The destination path upon a successful copy.
+    """
+
     src = pathify(src)
     dst = pathify(dst)
+
+    if src == dst:
+        log.debug(f'Tried copying {src} to itself, skipping...')
+        return
 
     try:
         if not src.exists():
@@ -93,9 +138,27 @@ def move(
     not_exist_ok: Optional[bool] = False,
     noclobber: Optional[bool] = False,
     ignore: Optional[Callable[[str, list[str]], Sequence]] = None,
-) -> Path:
+) -> Path | None:
+    """
+    Move a file or directory.
+
+    Args:
+        src: The source file.
+        dst: The destination file.
+        not_exist_ok: Do not throw an error if the source does not exist.
+        noclobber: Throw an error if the destination exists (i.e. do not overwrite files).
+        ignore: A callable that is passed to the `ignore` argument of `shutil.copytree()`.
+
+    Returns:
+        The destination path upon a successful move.
+    """
+
     src = pathify(src)
     dst = pathify(dst)
+
+    if src == dst:
+        log.debug(f'Tried moving {src} to itself, skipping...')
+        return
 
     try:
         if not src.exists():
