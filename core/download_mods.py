9e846bb87441577c5cae68904e9b2b7cb52cd716
djsigmann <djsigmann@protonmail.com>
2026-01-12 01:37:42 +0100

    fixup! refactor(core,util,gui): various small refactors

--- core/download_mods.py
+++ core/download_mods.py
@@ -40,13 +40,14 @@ def check_mods() -> Update | None:
     # - There was also a time where the mods were kept in a zip file checked into the VCS...yeah, ~80 MB...per revision...
 
     modsinfo = None
+    modsinfo_file = folder_setup.project_dir / 'modsinfo.json'
     try:
-        with (folder_setup.project_dir / 'modsinfo.json').open('r') as fd:
+        with modsinfo_file.open('r') as fd:
             modsinfo = json.load(fd)
     except FileNotFoundError:
         pass
     except json.JSONDecodeError:
-        log.exception(f'Could not parse {folder_setup.modsinfo_file}') # INFO: ignore this error and act as if the file didn't exist at all
+        log.exception(f'Could not parse {modsinfo_file}') # ignore this error and act as if the file didn't exist at all
 
     for update in get_releases_with_asset(REMOTE_REPO, 'mods.zip'):
         if modsinfo:
