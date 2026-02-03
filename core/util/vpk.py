fd61eeb96d03cac796bd38af0a905f9aef5d1319
cueki <github2@madisonlovett.dev>
2026-02-03 10:09:22 -0800

    chore: update smissmass particles

--- core/util/vpk.py
+++ core/util/vpk.py
@@ -2,11 +2,5 @@ from pathlib import Path
 
 
 def get_vpk_name(game_path: Path | str) -> str:
-    """Find the main *_misc_dir.vpk file for a Source mod."""
-    path = Path(game_path)
-
-    misc_vpks = list(path.glob("*_misc_dir.vpk"))
-    if misc_vpks:
-        return misc_vpks[0].name
-
+    """Find the main tf2_misc_dir.vpk file."""
     return "tf2_misc_dir.vpk"
