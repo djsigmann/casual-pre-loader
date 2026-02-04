4440555eb1c8b5be791d2464c4f4ec837ccb3fe9
cueki <github2@madisonlovett.dev>
2026-02-03 23:37:19 -0800

    fix: update RUNME.bat to match renamed build directory

--- scripts/RUNME.bat
+++ scripts/RUNME.bat
@@ -13,6 +13,6 @@ if /I "%~nx0"=="RUNME.bat" (
     )
 )
 
-cd casual-pre-loader
+cd casual-preloader
 python.exe pip.pyz install --disable-pip-version-check --no-warn-script-location -r requirements.txt
 python.exe main.py
