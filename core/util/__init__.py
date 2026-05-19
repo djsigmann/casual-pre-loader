d1c66e192c4dff816a2d2d166f814ff3e63f0d3b
djsigmann <djsigmann@protonmail.com>
2026-05-19 14:12:29 +0200

    feat: add functions for manipulating dicts and dataclasses to `util`

--- core/util/__init__.py
+++ core/util/__init__.py
@@ -1,5 +1,11 @@
-from collections.abc import Callable
-from typing import Any
+from collections.abc import Callable, Iterable
+from dataclasses import Field
+from typing import Any, ClassVar, Protocol, runtime_checkable
+
+
+@runtime_checkable
+class DataClass(Protocol):
+    __dataclass_fields__: ClassVar[dict[str, Field[Any]]]
 
 
 def all_predicates(*preds: Callable[[Any], bool]) -> Callable[[Any], bool]:
@@ -15,3 +21,15 @@ def all_predicates(*preds: Callable[[Any], bool]) -> Callable[[Any], bool]:
     """
 
     return lambda x: all(p(x) for p in preds)
+
+
+def filter_dict_keys[T](d: dict[T, Any], keys: Iterable[T]) -> dict[T, Any]:
+    return {
+        key: d[key]
+        for key in keys
+        if key in d
+    }
+
+
+def update_dataclass[T: DataClass](obj: T, *args, **kwargs) -> None:
+    obj.__dict__.update(*args, **kwargs)
