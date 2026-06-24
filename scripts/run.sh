6eeab908acdddd193a7251d0fe03aab164b3acbd
djsigmann <djsigmann@protonmail.com>
2026-06-24 14:03:16 +0200

    refactor: improve linux run script
    - Unsilence pip.
    - Make python version checking quicker by avoiding invoking a python
      interpreter.
    - Simply warn the user if wine is not installed instead of prompting
      them to continue.
    - Use braces where appropriate.
    - Create venvs using `--system-site-packages`. This uses existing global
      packages if they're present instead of downloading and installing them
      to the venv. This saves on drive space, time, and allows QT plugins
      that aren't packaged on PyPA (e.g. `qtct6`) to be used from within
      the venv.

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+MIN_PYTHON_VERSION=3.12
+
 # shellcheck disable=SC2312
 cd "$(dirname "$(dirname "$(realpath -s "${0}")")")" # one dir up
 
@@ -33,112 +35,144 @@ done
 unset level _level
 : $((max_len_level += 2)) # apply 2 spaces of padding
 
-dep_missing() { printf '%s is not installed, please install it using your package manager\n' "${1}"; }
+dep_missing() { printf '%s is not installed, please install it according to your distro\n' "${1}"; }
 
-prompt() (
-	[ -t 0 ] || return 1
-
-	printf '%s' "${1}" >&2
-	read -r REPLY
-	printf '\n' >&2
-
-	printf '%s' "${REPLY}"
-)
-
-prompt_yn() {
-	! [ -t 0 ] && printf n && return
-
-	set -- "${1}" "${2:-y}"
-	# shellcheck disable=SC2015
-	[ "${2}" = y ] &&
-		set -- "${1} [Y/n]" "${2}" ||
-		set -- "${1} [y/N]" "${2}"
-
-	case "$(prompt "${1}")" in
-	[yY]) printf y ;;
-	[nN]) printf n ;;
-	*) printf '%s' "${2}" ;;
-	esac
+intcmp() {
+	printf '%s' "$(( ($1 > $2) - ($1 < $2) ))"
 }
 
-check_python_version() {
-	python3 -c 'import sys; vi = sys.version_info; exit(not (vi.major == 3 and vi.minor >= 12))'
+check_min_python_version() (
+	set -e
+
+	version="$(python -V)" version="${version#Python }"
+
+	major="${1%%.*}"
+	_major="${version%%.*}"
+
+	ret=true
+	# shellcheck disable=SC2249
+	case "$(intcmp "${_major:-0}" "${major:-0}")" in
+		0)
+			rest="${1#"${major}"}" rest="${rest#.}"
+			minor="${rest%%.*}"
+			_rest="${version#"${_major}"}" _rest="${_rest#.}"
+			_minor="${_rest%%.*}"
+			case "$(intcmp "${_minor:-0}" "${minor:-0}")" in
+				0)
+					patch="${rest#"${minor}"}" patch="${patch#.}"
+					_patch="${_rest#"${_minor}"}" _patch="${_patch#.}"
+					case "$(intcmp "${_patch:-0}" "${patch:-0}")" in
+						-1) ret=false;;
+					esac
+					;;
+				-1) ret=false;;
+			esac
+			;;
+		-1) ret=false;;
+	esac
+
+	printf '%s' "${version}"
+
+	${ret}
+)
+
+mkvenv() { python3 -m venv --system-site-packages .venv; }
+
+# shellcheck disable=SC2312
+[ "$(id -u)" -eq 0 ] && {
+	printf "This script should not be run as root\n" | error
+	exit 1
 }
 
 ERROR=false
 
-# shellcheck disable=SC2310,SC2312
-[ "$(id -u)" -eq 0 ] && printf "This script should not be run as root\n" | error && ERROR=true
-
-# shellcheck disable=SC2310
 (
 	set -e
 
-	! command -v python3 >/dev/null 2>&1 &&
-		dep_missing python3 | error && false # none of the other commands in this subshell will work without python
+	# shellcheck disable=SC2310
+	command -v python3 >/dev/null 2>&1 || {
+		dep_missing python3 | error
+		false # none of the other commands in this subshell will work without python
+	}
 
-	# shellcheck disable=SC2312
-	! check_python_version && ERROR=true &&
-		printf 'Your version of python (%s) is out of date, the minimum required version is Python 3.12\n' \
-			"$(python3 -V)" | error
+	# shellcheck disable=SC2310
+	pyver="$(check_min_python_version "${MIN_PYTHON_VERSION}")" || {
+		ERROR=true
+		printf 'Your version of python (%s) is out of date, the minimum required version is Python %s\n' "${pyver}" "${MIN_PYTHON_VERSION}" | error
+	}
 
-	! python3 -m ensurepip --version >/dev/null 2>&1 && ERROR=true &&
+	# shellcheck disable=SC2310
+	python3 -m ensurepip --version >/dev/null 2>&1 || {
+		ERROR=true
 		dep_missing ensurepip | error
+	}
 
-	! python3 -c 'import venv' 2>/dev/null && ERROR=true &&
+	# shellcheck disable=SC2310
+	python3 -c 'import venv' 2>/dev/null || {
+		ERROR=true
 		dep_missing 'python3-venv' | error
+	}
 
 	! ${ERROR}
 ) || ERROR=true
 
-# shellcheck disable=SC2310,SC2312
 # check for wine
-! command -v wine >/dev/null 2>&1 &&
-	dep_missing wine | warning &&
-	printf '%s\n' 'Wine is required to run studiomdl.exe for model precaching' | warning &&
-	{ ${ERROR} || [ "$(prompt_yn 'Continue anyway?' n)" != y ]; } && ERROR=true
+command -v wine >/dev/null 2>&1 ||
+	printf '%s\n' 'Wine is required to run studiomdl.exe for model precaching' | warning
 
-${ERROR} && exit 1 # exit if errors were previously raised
-
-# try to ensure that submodules ARE in fact, properly cloned
-! git submodule update --init --recursive --remote &&
-	ERROR=true &&
-	printf 'No internet connection, cannot update submodules\n' | error
+! ${ERROR} || exit 1 # exit if errors were previously raised
 
 if [ -f 'requirements.txt' ]; then
-	# shellcheck disable=SC2310,SC2312
-	! [ -f '.venv/bin/activate' ] &&
-		printf '%s\n' 'Creating virtual environment' | info &&
-		python3 -m venv .venv
+	[ -f '.venv/bin/activate' ] || {
+		printf '%s\n' 'Creating virtual environment' | info
+		mkvenv
+	}
 
 	. .venv/bin/activate
 
 	# shellcheck disable=SC2310
-	if ! check_python_version; then
-		printf '%s\n' 'virtual environment is using an out-of-date version of python, attempting to recreate' | warning
+	if ! pyver="$(check_min_python_version "${MIN_PYTHON_VERSION}")"; then
+		printf "The virtual environment's version of python (%s) is out of date, the minimum required version is Python %s\n" "${pyver}" "${MIN_PYTHON_VERSION}" | warning
 
 		# shellcheck disable=SC2218
-		deactivate
+		deactivate # defined by venv
 
 		rm -r .venv
-		python3 -m venv .venv
+		mkvenv
 		. .venv/bin/activate
 
-		! check_python_version &&
-			printf '%s\n' 'unable to recreate the virtual environment with an up-to-date version of python' | error &&
+		check_min_python_version "${MIN_PYTHON_VERSION}" || {
+			printf '%s\n' 'unable to recreate the virtual environment with an up-to-date version of python' | error
 			exit 1
+		}
+
 		printf '%s\n' 'managed to recreate the virtual environment with an up-to-date version of python' | warning
 	fi
 
-	if ${ERROR}; then
-		printf 'No internet connection, cannot install and/or update dependencies\n' | error
-	else
-		printf '%s\n' 'Installing and/or updating dependencies' | info
-		python3 -m ensurepip
-		python3 -m pip -q install --upgrade pip
-		python3 -m pip -q install --upgrade -r requirements.txt
-	fi
+	printf '%s\n' 'Installing and/or updating dependencies' | info
+	export PIP_RETRIES="${PIP_RETRIES:-2}" PIP_TIMEOUT="${PIP_TIMEOUT:-5}"
+	{
+		python3 -m ensurepip &&
+		python3 -m pip install --upgrade pip &&
+		python3 -m pip install --upgrade -r requirements.txt
+	} || {
+		printf 'No internet connection, cannot install and/or update required dependencies\n' | error
+		ERROR=true
+	}
 fi
+
+# ensure that submodules ARE in fact, properly cloned
+git submodule update --init --recursive --remote || {
+	# shellcheck disable=SC2016
+	git submodule update --init --recursive &&
+	git submodule foreach --recursive  '[ "${sha1}" = "$(git rev-parse HEAD)" ]' &&
+	printf 'No internet connection, but submodules seem to be up-to-date, it is advised to retry once an internet connection has been re-established\n' | warning
+} || {
+	ERROR=true
+	printf 'No internet connection, cannot update submodules\n' | error
+}
+
+! ${ERROR} || exit 1
 
 printf '%s\n' 'Starting Casual Preloader' | info
 exec ./main.py "${@}"
